dccf121698df94dbc4ebedf240c4727efa0f89de
Allanpt <jatallantavares@gmail.com>
2023-07-13 09:32:54 -0300

    Navbar finished

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburguer-menu").addEventListener("click", function() {
+
+    let divisor = document.getElementById("divisor");
+    divisor.parentNode.removeChild(divisor);
+  });
+  
