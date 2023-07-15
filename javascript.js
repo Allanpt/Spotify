b8d3b8a9c5572dc6161f12ae410b8bcd4d00ece1
Allanpt <jatallantavares@gmail.com>
2023-07-15 10:19:16 -0300

    Finalizção da Capa e começo do "about-spotify"

--- javascript.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("hamburguer-menu").addEventListener("click", function() {
-
-    let divisor = document.getElementById("divisor");
-    divisor.parentNode.removeChild(divisor);
-  });
-  
